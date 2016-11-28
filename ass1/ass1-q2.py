7224d506a3751adf0b6ffb0c05e1288bdb50862a
srimuthu <muthu171191@gmail.com>
2016-11-28 12:21:57 +0100

    base data structures

--- ass1/ass1-q2.py
+++ ass1/ass1-q2.py
@@ -2,81 +2,21 @@ from z3 import *
 import utils
 
 
-TRUCKS = 8
-SLOTS = 8
-CAPACITY = 8000
+CHIP_WIDTH = 30
+CHIP_HEIGHT = 30
+COMPONENT_DIM = [(4,3),(4,3),(4,5),(4,6),(5,20),(6,9),(6,10),
+					(6,11),(7,8),(7,12),(10,10),(10,20)]
 
-ITEM_TYPES = 5
+RESULT_PARAMS = ['X','Y','Width','Height']
+PARAM = {}
 
-NUZZLES = 4
-NUZZLE_WEIGHT = 700
+for idx, par in enumerate(RESULT_PARAMS):
+	PARAM[par] = idx
 
-PRITTLES = 22 # variable, so far 22 is the maximum satisfiable
-PRITTLE_WEIGHT = 400
 
-SKIPPLES = 8
-SKIPPLE_WEIGHT = 1000
+CHIP_POS = [[Real('comp%s%s' % (i,RESULT_PARAMS[j])) for j in range(len(RESULT_PARAMS))]
+			 for i in range(len(COMPONENT_DIM))]
 
-CROTTLES = 10
-CROTTLE_WEIGHT = 2500
+# print(CHIP_POS)
 
-DUPPLES = 20
-DUPPLE_WEIGHT = 200
-
-ITEMS  = ['Nuzzle', 'Prittle', 'Skipple', 'Crottle', 'Dupple']
-WEIGHT = [NUZZLE_WEIGHT, PRITTLE_WEIGHT, SKIPPLE_WEIGHT, CROTTLE_WEIGHT, DUPPLE_WEIGHT]
-DEMAND = [NUZZLES, PRITTLES, SKIPPLES, CROTTLES, DUPPLES]
-
-TYPEOF = {}
-for idx, item in enumerate(ITEMS):
-	TYPEOF[item] = idx
-
-# print(DEMAND)
-
-# print('truck%s%sCount' % (0, items[1]))
-truckItem = [[ Int('truck%s%sCount' % (i, ITEMS[j])) for j in range(ITEM_TYPES) ] for i in range(TRUCKS)]
-
-# print(truckItem)
 s = Solver()
-
-for i in range (TRUCKS):
-	# All trucks have at least 0 of each item type
-	s.add([truckItem[i][j] >= 0  for j in range (ITEM_TYPES)])
-
-	# The number of items on a truck is less than its number of available slots
-	s.add(Sum(truckItem[i]) <= SLOTS)
-
-	# The combined weight of all items on a truck is less than its capacity
-	s.add(Sum([
-		truckItem[i][j] * WEIGHT[j] for j in range(ITEM_TYPES)
-	]) <= CAPACITY)
-
-# The combined number of items delivered by all trucks per item type must be equal to the demand 
-for j in range(ITEM_TYPES):
-	s.add(
-		Sum(
-			[ truckItem[i][j] for i in range(TRUCKS) ]
-		) == DEMAND[j]
-	)
-
-# Only the first 3 trucks can store skipples
-for i in range(3, TRUCKS):
-	s.add(
-		truckItem[i][TYPEOF['Skipple']] == 0
-	)
-
-# At most 1 nuzzle per truck
-for i in range(TRUCKS):
-	s.add(
-		truckItem[i][TYPEOF['Nuzzle']] <= 1
-	)
-
-# print(s.check())
-
-if s.check() == sat:
-	print(s.model())
-	utils.z3_to_smt2(s, "ass1-q1")
-
-else:
-	print("Failed to solve")
-# print(s.to_smt2())
